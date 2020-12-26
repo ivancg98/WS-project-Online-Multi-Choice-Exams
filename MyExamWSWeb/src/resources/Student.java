5b23122794be80f6240b3d92e53f7ce6733fad4f
AitorZaragoza <aitorzag@gmail.com>
2020-12-26 11:14:22 +0100

    Student

---
+++ MyExamWSWeb/src/resources/Student.java
@@ -0,0 +1,34 @@
+package resources;
+
+public class Student {
+
+	private int key;
+	private String studentId;
+	
+	public Student(String studentId) {
+		this.setStudentId(studentId);
+	}
+	
+	public Student(int key, String studentId) {
+		this.setKey(key);
+		this.setStudentId(studentId);
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+}

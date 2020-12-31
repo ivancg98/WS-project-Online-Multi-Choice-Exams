3e27516a60df81e7666635be37417e64af51d0a3
Ivan <icortes.digital@gmail.com>
2020-12-31 16:12:47 +0100

    implement Client class

--- myExamwsWeb/src/entities/Client.java
+++ myExamwsWeb/src/entities/Client.java
@@ -1,5 +1,56 @@
 package entities;
 
 public class Client {
+	public String key;
+	public int examKey;
+	public String descriptionExam;
+	public float grade;
+	
 
+	public Client() {
+		
+	}
+	
+	public Client(String key, int examKey, String descriptionExam, float grade) {
+		this.key = key;
+		this.examKey= examKey;
+		this.descriptionExam = descriptionExam;
+		this.grade = grade;
+	}
+	
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public void setKeyExam(int examKey) {
+		this.examKey = examKey;
+	}
+	
+	public void setDescriptionExam(String descriptionExam) {
+		this.descriptionExam = descriptionExam;
+	}
+	
+	public void setGrade(float grade) {
+		this.grade = grade;
+	}
+	
+	public String getKey() {
+		return this.key;
+	}
+	
+	public int getExamKey() {
+		return this.examKey;
+	}
+	
+	public String getDescriptionExam() {
+		return this.descriptionExam;
+	}
+	
+	public float getGrade() {
+		return this.grade;
+	}
+	
+	
+	
 }

7458f89c01e13fd01e540c8f7e8e29e49cb29433
Ivan <icortes.digital@gmail.com>
2020-12-31 17:57:22 +0100

    Edit class Client

--- myExamwsWeb/src/entities/Client.java
+++ myExamwsWeb/src/entities/Client.java
@@ -3,7 +3,6 @@ package entities;
 public class Client {
 	public String key;
 	public int examKey;
-	public String descriptionExam;
 	public float grade;
 	
 
@@ -11,10 +10,9 @@ public class Client {
 		
 	}
 	
-	public Client(String key, int examKey, String descriptionExam, float grade) {
+	public Client(String key, int examKey, float grade) {
 		this.key = key;
 		this.examKey= examKey;
-		this.descriptionExam = descriptionExam;
 		this.grade = grade;
 	}
 	
@@ -26,11 +24,7 @@ public class Client {
 	public void setKeyExam(int examKey) {
 		this.examKey = examKey;
 	}
-	
-	public void setDescriptionExam(String descriptionExam) {
-		this.descriptionExam = descriptionExam;
-	}
-	
+		
 	public void setGrade(float grade) {
 		this.grade = grade;
 	}
@@ -41,10 +35,6 @@ public class Client {
 	
 	public int getExamKey() {
 		return this.examKey;
-	}
-	
-	public String getDescriptionExam() {
-		return this.descriptionExam;
 	}
 	
 	public float getGrade() {
